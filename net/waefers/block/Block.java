057baf0238b3eb05b6f082a95bea8bee878aa8c4
michael.ansel <michael.ansel@a78de98e-401f-0410-8945-8120b6f64072>
2006-10-12 15:46:15 +0000

    Set up Block object Working on LocationMessage and ReplicaMaster

---
+++ net/waefers/block/Block.java
@@ -0,0 +1,70 @@
+package net.waefers.block;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+/**
+ * 
+ * Represents a data block
+ * 
+ * @author Michael Ansel
+ */
+
+import net.waefers.block.BlockControl;
+import static net.waefers.GlobalControl.log;
+
+public class Block {
+	
+	/**
+	 * Possible block types
+	 * DATA: Block contains the actual data
+	 * REFERENCE: Block contains a reference to a local file containing the data
+	 */
+	private enum BlockType {DATA,REFERENCE};
+	
+	/**
+	 * Type of block
+	 */
+	private BlockType type;
+	
+	/**
+	 * Block data
+	 */
+	private ByteBuffer data;
+	
+	/**
+	 * Data hash (id)
+	 */
+	public int id;
+	
+	/**
+	 * 
+	 * @return Whether or not the toggle was successful
+	 */
+	public boolean toggleType() {
+		try {
+			if(type==BlockType.DATA) {
+				type = BlockType.REFERENCE;
+				boolean work = BlockControl.writeFile(BlockControl.dataFile(id),data);
+				if(work) data=null;
+				return work;
+			}
+			if(type==BlockType.REFERENCE) {
+				type = BlockType.DATA;
+				data = BlockControl.readFile(BlockControl.dataFile(id));
+				return data!=null;
+			}
+			return false;
+		} catch(IOException e) {
+			log.throwing("Block", "toggleType", e);
+			return false;
+		}
+	}
+	
+	public ByteBuffer getData() {
+		if(type==BlockType.REFERENCE) this.toggleType();
+		
+		return data;
+	}
+
+}
---
+++ net/waefers/block/BlockControl.java
@@ -0,0 +1,43 @@
+package net.waefers.block;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+public class BlockControl {
+	
+	/**
+	 * 
+	 * Returns the local data file for a selected hash
+	 * 
+	 * @param id data hash/id
+	 * @return Local data file
+	 */
+	public static File dataFile(int id) {
+		return null;
+	}
+	
+	/**
+	 * 
+	 * Reads data from local file with verification
+	 * 
+	 * @param file File to read data from
+	 * @return Data from file
+	 */
+	public static ByteBuffer readFile(File file)  throws IOException {
+		return null;
+	}
+	
+	/**
+	 * 
+	 * Writes data to local File with verification
+	 * 
+	 * @param file File to write data to
+	 * @param data Data to write to file
+	 * @return Whether or not the write action was successful
+	 * @throws IOException
+	 */
+	public static boolean writeFile(File file, ByteBuffer data) throws IOException {
+		return false;
+	}
+}
