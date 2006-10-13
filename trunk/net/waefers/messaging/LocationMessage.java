607b4485fc5f7ac2d901238cd70e816e68bfac11
michael.ansel <michael.ansel@a78de98e-401f-0410-8945-8120b6f64072>
2006-10-13 15:48:05 +0000

    Working on ReplicaMaster

--- trunk/net/waefers/messaging/LocationMessage.java
+++ trunk/net/waefers/messaging/LocationMessage.java
@@ -2,13 +2,14 @@ package net.waefers.messaging;
 
 import java.util.HashSet;
 import net.waefers.block.Block;
+import net.waefers.node.Node;
 
 public class LocationMessage {
 	
 	/**
 	 * Possible actions to perform on replica directory
 	 */
-	public enum LocationAction {ADD,GET,REMOVE};
+	public enum LocationAction {ADD,GET,REMOVE_PART,REMOVE_FULL};
 	
 	/**
 	 * Action to perform on replica directory
@@ -18,8 +19,10 @@ public class LocationMessage {
 	/**
 	 * Set with all local data in it, or references to location on disk
 	 */
-	HashSet<Block> blocks = new HashSet<Block>();
+	public HashSet<Block> blocks = new HashSet<Block>();
 	
-	
-	
+	/**
+	 * Node to associate/associated with blocks
+	 */
+	public Node node;
 }
